03a23a483286d5993655555c5d32593c16d61442
Müge Yilmaz <mge_304@icloud.com>
2024-08-16 10:45:35 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        dikdörtgencevrealan dikdortgen = new dikdörtgencevrealan(10, 5);
+
+
+        double alan = dikdortgen.alanHesapla();
+        double cevre = dikdortgen.cevreHesapla();
+
+
+        System.out.println("Dikdörtgenin Alanı: " + alan + " birim kare");
+        System.out.println("Dikdörtgenin Çevresi: " + cevre + " birim");
     }
 }
--- src/dikdörtgencevrealan.java
+++ src/dikdörtgencevrealan.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class dikdörtgencevrealan {
+
+
+
+        private double uzunKenar;
+        private double kisaKenar;
+
+
+        public dikdörtgencevrealan(double uzunKenar, double kisaKenar) {
+            this.uzunKenar = uzunKenar;
+            this.kisaKenar = kisaKenar;
+        }
+
+
+        public double getUzunKenar() {
+            return uzunKenar;
+        }
+
+        public void setUzunKenar(double uzunKenar) {
+            this.uzunKenar = uzunKenar;
+        }
+
+        public double getKisaKenar() {
+            return kisaKenar;
+        }
+
+        public void setKisaKenar(double kisaKenar) {
+            this.kisaKenar = kisaKenar;
+        }
+
+
+        public double alanHesapla() {
+            return uzunKenar * kisaKenar;
+        }
+
+
+        public double cevreHesapla() {
+            return 2 * (uzunKenar + kisaKenar);
+        }
+
+
+
 }
